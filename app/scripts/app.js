1710a247087b977d6e33c26f01855053d8ba6eac
andrew.white <andrew.white@spdatallc.com>
2017-08-16 18:41:21 -0400

    Added new sevices and directive scafolds, adjusted linking style, Added angular-strap. EOD 8-16-17

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,36 @@
  * Main module of the application.
  */
 angular
-  .module('faqizerApp', []);
+  .module('faqizerApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'mgcrea.ngStrap'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl',
+        controllerAs: 'contact'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+     $locationProvider.html5Mode(true);
+  });
